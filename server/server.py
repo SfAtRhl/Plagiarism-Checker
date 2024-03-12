dc3c3c60e332f2428d961f67f177759a3883a3c8
SfAtRhl <soufyane.aitrehail@gmail.com>
2024-03-12 10:20:16 +0000

    Done

---
+++ server/server.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+import os
+import PyPDF2
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from arango import ArangoClient
+
+app = Flask(__name__)
+CORS(app)
+
+# Initialize ArangoDB connection
+client = ArangoClient()
+sys_db = client.db('_system', username='root', password='root')
+db_name = 'Plagiarism'
+collection_name = 'results_collection'
+
+if not sys_db.has_database(db_name):
+    sys_db.create_database(db_name)
+
+db = client.db(db_name, username='root', password='root')
+
+if not db.has_collection(collection_name):
+    db.create_collection(collection_name)
+
+def extract_text_from_pdf(file):
+    pdf_text = ""
+    pdf_reader = PyPDF2.PdfReader(file)
+    
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        pdf_text += page.extract_text()
+
+    return pdf_text
+
+
+def vectorize(Text):
+    return TfidfVectorizer().fit_transform(Text).toarray()
+
+
+def similarity(doc1, doc2):
+    return cosine_similarity([doc1, doc2])
+
+
+def check_plagiarism(text_vector_a, text_vector_b, student_a, student_b):
+    sim_score = similarity(text_vector_a, text_vector_b)[0][1]
+
+    result = {
+        'document1': student_a,
+        # 'content1': text_vector_a,
+        'document2': student_b,
+        # 'content2': text_vector_b,
+        'similarity_score': sim_score
+    }
+    return result
+
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    uploaded_files = request.files.getlist('file')
+    uploaded_texts = []
+
+    for file in uploaded_files:
+        if file.filename.endswith('.pdf'):
+            content = extract_text_from_pdf(file)
+        else:
+            content = file.read().decode('utf-8')
+        uploaded_texts.append(content)
+
+    vectors = vectorize(uploaded_texts)
+    s_vectors = list(zip(uploaded_files, vectors))
+
+    # print(uploaded_texts)
+
+    plagiarism_results = []
+
+    for student_a, text_vector_a in s_vectors:
+        new_vectors = s_vectors.copy()
+        current_index = new_vectors.index((student_a, text_vector_a))
+        del new_vectors[current_index]
+        for student_b, text_vector_b in new_vectors:
+            result = check_plagiarism(text_vector_a, text_vector_b, student_a.filename, student_b.filename)
+            plagiarism_results.append(result)
+
+            # Store the results in ArangoDB collection
+            print(plagiarism_results[0])
+            db[collection_name].insert(plagiarism_results[:1])
+    return jsonify({'plagiarism_results': plagiarism_results})
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { motion } from "framer-motion";
 
-function App() {
+const App = () => {
+  const [file1, setFile1] = useState(null);
+  const [file2, setFile2] = useState(null);
+  const [similarityResults, setSimilarityResults] = useState(null);
+
+  const handleFile1Change = (e) => {
+    setFile1(e.target.files[0]);
+  };
+
+  const handleFile2Change = (e) => {
+    setFile2(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", file1);
+    formData.append("file", file2);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setSimilarityResults(response.data.plagiarism_results);
+    } catch (error) {
+      console.error("Error uploading files:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mx-auto p-4 flex flex-col justify-center items-center text-white h-screen ">
+      <h1 className="text-4xl font-bold mb-3 text-center">
+        Plagiarism Checker
+      </h1>
+      <p className="text-sm md:text-lg font-normal md:font-medium mb-8 text-center">
+        A Plagiarism Checker scans and evaluates content for unauthorized
+        copying, ensuring originality.
+      </p>
+
+      <div className="flex flex-col md:flex-row justify-between mx-auto gap-4">
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-medium">File 1:</label>
+          <input
+            type="file"
+            onChange={handleFile1Change}
+            className="file-input"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-medium">File 2:</label>
+          <input
+            type="file"
+            onChange={handleFile2Change}
+            className="file-input"
+          />
+        </div>
+      </div>
+
+      <motion.button
+        whileHover={{ scale: 1.04 }}
+        whileTap={{ scale: 0.95 }}
+        onClick={handleUpload}
+        className="bg-gray-500 text-white px-4 py-2 rounded mt-4"
+      >
+        Check Plagiarism
+      </motion.button>
+
+      {similarityResults && (
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="mt-6"
         >
-          Learn React
-        </a>
-      </header>
+          <h2 className="text-2xl font-bold mb-2">Similarity Results:</h2>
+          <ul>
+            {similarityResults.map((result, index) => (
+              <li key={index} className="mb-2 text-lg">
+                {result.document1} and {result.document2}: Similarity Score -{" "}
+                {result.similarity_score.toFixed(2)}
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
